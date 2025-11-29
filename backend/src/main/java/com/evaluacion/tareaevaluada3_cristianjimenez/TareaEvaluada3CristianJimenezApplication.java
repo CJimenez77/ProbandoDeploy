f7432f4daba357831c5c5e71637911a20a1826a1
Cristian <cristian.jimenez2201@alumnos.ubiobio.cl>
2025-11-29 12:28:44 -0300

    Primer commit: Estructura de proyecto, carpeta backend y carpeta frontend, en backend esta lo generado automaticamente por Spring initializr

---
+++ backend/src/main/java/com/evaluacion/tareaevaluada3_cristianjimenez/TareaEvaluada3CristianJimenezApplication.java
@@ -0,0 +1,13 @@
+package com.evaluacion.tareaevaluada3_cristianjimenez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaEvaluada3CristianJimenezApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TareaEvaluada3CristianJimenezApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/com/evaluacion/tareaevaluada3_cristianjimenez/TareaEvaluada3CristianJimenezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evaluacion.tareaevaluada3_cristianjimenez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaEvaluada3CristianJimenezApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
